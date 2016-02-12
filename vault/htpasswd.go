815dc7698a7a73c5e29c4157fa7f06e8962fd010
Jan Halfar <jan@bestbytes.com>
2016-02-12 19:46:25 +0100

    mving htpasswd impl

--- vault/htpasswd.go
+++ vault/htpasswd.go
@@ -4,8 +4,6 @@ import (
 	"io/ioutil"
 
 	"gopkg.in/yaml.v2"
-
-	"strings"
 )
 
 // ReadHtpasswdConfigFromFile read htpasswd config from a file
@@ -20,22 +18,4 @@ func ReadHtpasswdConfigFromFile(filename string) (config map[string][]string, er
 func parseHtpasswdConfig(configBytes []byte) (config map[string][]string, err error) {
 	config = make(map[string][]string)
 	return config, yaml.Unmarshal(configBytes, config)
-}
-
-func parseHtpasswd(htpasswdBytes []byte) (passwords map[string]string, err error) {
-	lines := strings.Split("\n", string(htpasswdBytes))
-	passwords = make(map[string]string)
-	for _, line := range lines {
-		// scan lines
-		line = strings.Trim(line, " ")
-		if len(line) == 0 {
-			// skipping empty lines
-			continue
-		}
-		parts := strings.Split(":", line)
-		for i, part := range parts {
-			parts[i] = strings.Trim(part, " ")
-		}
-	}
-	return
 }
