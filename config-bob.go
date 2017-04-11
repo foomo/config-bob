4699bd8f9232ee09bd195d2250264369dfb4d775
Stefan Martinov <smartinov@mbp.local>
2017-04-11 13:53:28 +0200

    remove break statement from switch clause (un-needed)

--- config-bob.go
+++ config-bob.go
@@ -231,19 +231,14 @@ func main() {
 		switch os.Args[1] {
 		case commandVersion:
 			versionCommand()
-			break
 		case commandVaultTree:
 			vaultTreeCommand()
-			break
 		case commandHtpasswd:
 			htpasswdCommand()
-			break
 		case commandVaultLocal:
 			vaultLocalCommand()
-			break
 		case commandBuild:
 			buildCommand()
-			break
 		default:
 			fmt.Println("unknown command", "\""+os.Args[1]+"\"")
 			help()
